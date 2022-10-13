8c50f530f42e0cddcff71f8e13ad5fb2f3302b9f
LukasD <lukas.dvarionis@gmail.com>
2022-10-13 17:56:25 +0300

    Initial commit

---
+++ src/test/java/com/example/Meetings_API/MeetingsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Meetings_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
